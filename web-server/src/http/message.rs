8c7146a97ab42dac9553a745d223baaf5a9d595c
Ole Martin Ruud <barskern@outlook.com>
2018-04-03 22:04:14 +0200

    [web-server] Change body of  to be &str

--- web-server/src/http/message.rs
+++ web-server/src/http/message.rs
@@ -1,59 +1,57 @@
-use std;
 use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Message<'a> {
   headers: HashMap<&'a str, &'a str>,
-  body: Vec<&'a str>,
+  body: &'a str,
 }
 
 impl<'a> Message<'a> {
-  pub fn from_str(message_str: &'a str) -> Self {
-    let lines = message_str.lines();
-    Message::from_lines(lines)
-  }
-
-  pub fn from_lines(mut message_lines: std::str::Lines<'a>) -> Self {
+  pub fn from_str(message_str: &'a str) -> Option<Self> {
+    let mut lines = message_str.lines();
     let mut headers: HashMap<&'a str, &'a str> = HashMap::new();
-    while let Some(header_line) = message_lines.next() {
+    while let Some(header_line) = lines.next() {
       if header_line.is_empty() {
         // We found the start of the body!
         break;
       }
       let mut header_line_iter = header_line.splitn(2, ':').map(|s| s.trim());
-      let name = header_line_iter.next().unwrap();
-      let value = header_line_iter.next().unwrap();
+      let name = header_line_iter.next()?;
+      let value = header_line_iter.next()?;
 
       headers.insert(name, value);
     }
 
-    Message {
-      headers,
-      body: message_lines.collect(), // Collects remaining lines
-    }
+    let body = {
+      let newline_char_len = 2;
+      let remaining_bytes = lines.fold(0, |acc, line| acc + line.len() + newline_char_len);
+
+      if remaining_bytes < message_str.len() {
+        &message_str[message_str.len() - remaining_bytes..]
+      } else {
+        ""
+      }
+    };
+
+    Some(Message { headers, body })
   }
 }
 
 pub struct MessageBuilder<'a> {
   __headers: HashMap<&'a str, &'a str>,
-  __body: Vec<&'a str>,
+  __body: &'a str,
 }
 
 impl<'a> MessageBuilder<'a> {
   pub fn new() -> Self {
     MessageBuilder {
       __headers: HashMap::new(),
-      __body: Vec::new(),
+      __body: "",
     }
   }
 
   pub fn body(mut self, body: &'a str) -> Self {
-    self.__body = body.lines().collect();
-    self
-  }
-
-  pub fn add_lines_to_body(mut self, new_lines: Vec<&'a str>) -> Self {
-    self.__body.extend(new_lines);
+    self.__body = body;
     self
   }
 
--- web-server/src/http/request.rs
+++ web-server/src/http/request.rs
@@ -1,4 +1,4 @@
-use super::message::Message;
+use super::message::{Message, MessageBuilder};
 
 #[derive(Debug)]
 pub struct Request<'a> {
@@ -9,21 +9,33 @@ pub struct Request<'a> {
 }
 
 impl<'a> Request<'a> {
-  pub fn from_str(request_str: &'a str) -> Result<Self, String> {
+  pub fn from_str(request_str: &'a str) -> Option<Self> {
     let mut lines = request_str.lines();
 
-    let mut request_line = lines.next().unwrap().split(' ');
+    let mut request_line = lines.next()?.split(' ');
 
     let method = {
-      let method_str = request_line.next().unwrap();
-      Methods::from_str(method_str)?
+      let method_str = request_line.next()?;
+      if let Ok(m) = Methods::from_str(method_str) {
+        m
+      } else {
+        return None;
+      }
     };
-    let uri = request_line.next().unwrap();
-    let version = request_line.next().unwrap();
+    let uri = request_line.next()?;
+    let version = request_line.next()?;
 
-    let message = Message::from_lines(lines);
+    let message = {
+      let newline_char_len = 2;
+      let remaining_bytes = lines.fold(0, |acc, line| acc + line.len() + newline_char_len);
+      if let Some(m) = Message::from_str(&request_str[request_str.len() - remaining_bytes..]) {
+        m
+      } else {
+        MessageBuilder::new().build()
+      }
+    };
 
-    Ok(Request {
+    Some(Request {
       method,
       uri,
       version,
