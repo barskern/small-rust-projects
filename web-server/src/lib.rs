1c777ceb9ffb99ff8b8e4eceb6e00b1c86b5111d
Ole Martin Ruud <barskern@outlook.com>
2018-04-04 22:34:04 +0200

    [web-server] Add new way of parsing TcpStreams

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,16 +1,13 @@
 #![feature(try_from)]
 
 use std::net::TcpListener;
-use std::io::Read;
 use std::path::Path;
 
-mod utils;
 mod http;
 mod router;
+mod utils;
 
 use router::Router;
-
-const MAX_REQUEST_SIZE: usize = 1024;
 
 pub fn run(port: usize) {
   let dir_path = Path::new("./html/");
@@ -18,24 +15,25 @@ pub fn run(port: usize) {
 
   let listener = TcpListener::bind(format!("localhost:{}", port)).unwrap();
   println!("Listening for connections at port {}", port);
+
   for stream in listener.incoming() {
     match stream {
       Ok(mut stream) => {
-        let mut request_str = String::with_capacity(MAX_REQUEST_SIZE);
-        println!("Initialized request str");
-
-        let _n_bytes_read = stream
-          .take(MAX_REQUEST_SIZE as u64)
-          .read_to_string(&mut request_str)
-          .unwrap();
-
-        println!("{}", request_str);
-
-        /* let request: http::Request = request_str.parse().unwrap();
-        println!("{:#?}", request);
-        router.handle_request(request); */
+        match utils::read_string_from_stream(&mut stream) {
+          Ok(request_str) => {
+            println!("Recived request:\n{}", request_str);
+            /* let request: http::Request = request_str.parse().unwrap();
+            println!("{:#?}", request);
+            router.handle_request(request); */
+          }
+          Err(e) => {
+            /* Perhaps do some logging of failed requests? */
+            println!("Parsing of request failed: {}", e);
+          }
+        }
       }
       Err(e) => {
+        /* Perhaps do some logging of failed requests? */
         println!("Connection failed: {}", e);
       }
     }
--- web-server/src/utils.rs
+++ web-server/src/utils.rs
@@ -1,5 +1,12 @@
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use std::io::{self, Read};
+use std::string;
+use std::error;
+use std::fmt;
+use std::net::TcpStream;
+
+const MAX_REQUEST_SIZE: usize = 1024;
 
 /// Visits all files in from given dir to deepest nested subdir. Applies the function to all files.
 pub fn visit_dir<F>(dir_path: &Path, f: &mut F)
@@ -31,4 +38,59 @@ where
   files
     .into_iter()
     .for_each(|dir_entry| f(dir_entry, dir_depth));
+}
+
+#[derive(Debug)]
+/// An error which is a wrapper around possible errors on reading a TcpStream.
+pub enum ReadStreamError {
+  Io(io::Error),
+  Parse(string::FromUtf8Error),
+}
+
+impl fmt::Display for ReadStreamError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ReadStreamError::Io(ref err) => write!(f, "IO error: {}", err),
+            ReadStreamError::Parse(ref err) => write!(f, "Parse error: {}", err),
+        }
+    }
+}
+
+impl error::Error for ReadStreamError {
+    fn description(&self) -> &str {
+        match *self {
+            ReadStreamError::Io(ref err) => err.description(),
+            ReadStreamError::Parse(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            ReadStreamError::Io(ref err) => Some(err),
+            ReadStreamError::Parse(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<io::Error> for ReadStreamError {
+    fn from(err: io::Error) -> ReadStreamError {
+        ReadStreamError::Io(err)
+    }
+}
+
+impl From<string::FromUtf8Error> for ReadStreamError {
+    fn from(err: string::FromUtf8Error) -> ReadStreamError {
+        ReadStreamError::Parse(err)
+    }
+}
+
+/// Reads request from stream and returns a string which contains the request in UTF8
+pub fn read_string_from_stream(stream: &mut TcpStream) -> Result<String, ReadStreamError> {
+  let mut buffer: Box<[u8]> = Box::new([0; MAX_REQUEST_SIZE]);
+  let bytes_read = stream.read(&mut buffer)?;
+  let mut buffer_vec = buffer.into_vec();
+  buffer_vec.truncate(bytes_read);
+
+  let res = String::from_utf8(buffer_vec)?;
+  Ok(res)
 }
