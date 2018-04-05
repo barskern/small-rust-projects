b0e2319386ca8114b47f6c76fc301fc10f3920ee
Ole Martin Ruud <barskern@outlook.com>
2018-04-05 07:48:06 +0200

    [web-server] Rewrites how streams are handled

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -18,31 +18,18 @@ pub fn run(port: usize) {
   println!("Listening for connections at port {}", port);
 
   for stream in listener.incoming() {
-    /*     stream
-      .map_err(|e| eprintln!("Connection failed: {}", e))
-      .map(|mut stream| utils::read_string_from_stream(&mut stream))
-      .map_err(|e| eprintln!("Parsing string from TcpStream failed: {}", e))
-      .map(|request_str| http::Request::try_from(request_str)); */
-    match stream {
-      Ok(mut stream) => {
-        match utils::read_string_from_stream(&mut stream) {
-          Ok(request_str) => match http::Request::try_from(request_str) {
-            Ok(request) => {
-              println!("{:#?}", request);
-              router.handle_request(request);
-            }
-            Err(e) => eprintln!("Parsing string to Request failed: {}", e),
-          },
-          Err(e) => {
-            /* Perhaps do some logging of failed requests? */
-            eprintln!("Parsing string from TcpStream failed: {}", e);
-          }
-        }
+    let parsed_stream = stream
+      .map_err(utils::ReadStreamError::Io)
+      .and_then(|mut stream| utils::read_string_from_stream(&mut stream));
+
+    match parsed_stream {
+      Ok(request_str) => {
+        match http::Request::try_from(request_str) {
+          Ok(request) => router.handle_request(request),
+          Err(e) => eprintln!("Error parsing string to Request: {}", e),
+        };
       }
-      Err(e) => {
-        /* Perhaps do some logging of failed requests? */
-        eprintln!("Connection failed: {}", e);
-      }
+      Err(e) => eprintln!("Error parsing stream: {}", e),
     }
   }
 }
